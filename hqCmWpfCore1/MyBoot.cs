4e91fb030b736a8cbdc31ddf090c4b2ef4f8a228
HQ911 <918305298@qq.com>
2022-04-01 17:00:56 +0800

    V1

---
+++ hqCmWpfCore1/MyBoot.cs
@@ -0,0 +1,49 @@
+﻿using Caliburn.Micro;
+using hqCmWpfCore1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace hqCmWpfCore1
+{
+    public class MyBoot:BootstrapperBase
+    {
+        private SimpleContainer con;
+        public MyBoot()
+        {
+             Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override void Configure()
+        {
+            //base.Configure();
+            con = new SimpleContainer();
+            con.Singleton<IWindowManager,WindowManager>();
+            con.PerRequest<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return con.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        { 
+            return con.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            //con.BuildUp(instance);
+        }
+
+    }
+}
---
+++ hqCmWpfCore1/ViewModels/ShellViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hqCmWpfCore1.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        private string _title;
+        public string Title { 
+            get => _title;
+            set 
+            {
+                _title = value;
+                NotifyOfPropertyChange("Title");
+            }
+        }
+        public ShellViewModel()
+        {
+            Title = "Welcome to Caliburn Micro in .net 6 WPF";
+        }
+
+    }
+}
+
